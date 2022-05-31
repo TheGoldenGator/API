355727eae7dc09a6e8c0cf500b7f5354949b30a3
Max <maxnatural13@gmail.com>
2022-05-31 00:21:23 -0500

    changed to string instead of int

--- queries/channel_update.go
+++ queries/channel_update.go
@@ -3,7 +3,6 @@ package queries
 import (
 	"context"
 	"fmt"
-	"strconv"
 
 	"github.com/Mahcks/TheGoldenGator/database"
 	"github.com/Mahcks/TheGoldenGator/twitch"
@@ -11,14 +10,9 @@ import (
 )
 
 func ChannelUpdate(event twitch.EventSubChannelUpdateEvent) error {
-	id, err := strconv.Atoi(event.BroadcasterUserID)
-	if err != nil {
-		return err
-	}
-
 	result, err := database.Stream.UpdateOne(
 		context.Background(),
-		bson.M{"user_id": id},
+		bson.M{"user_id": event.BroadcasterUserID},
 		bson.M{"$set": bson.M{"stream_title": event.Title, "stream_game_name": event.CategoryName, "stream_game_id": event.CategoryID}},
 	)
 
@@ -26,6 +20,6 @@ func ChannelUpdate(event twitch.EventSubChannelUpdateEvent) error {
 		return err
 	}
 
-	fmt.Printf("Stream changed for %v: %v [%v:%v] changed: %v", event.BroadcasterUserLogin, event.Title, event.CategoryName, event.CategoryID, result.ModifiedCount)
+	fmt.Printf("[CHANNEL.UPDATE] Stream changed for %v: %v [%v:%v] changed: %v", event.BroadcasterUserLogin, event.Title, event.CategoryName, event.CategoryID, result.ModifiedCount)
 	return nil
 }
