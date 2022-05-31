5778bde36eb1659f7c73de7422a024175abc301d
Max <maxnatural13@gmail.com>
2022-05-31 00:12:11 -0500

    updated channel_update query

--- queries/channel_update.go
+++ queries/channel_update.go
@@ -19,11 +19,7 @@ func ChannelUpdate(event twitch.EventSubChannelUpdateEvent) error {
 	result, err := database.Stream.UpdateOne(
 		context.Background(),
 		bson.M{"user_id": id},
-		bson.M{
-			"stream_title":     event.Title,
-			"stream_game_name": event.CategoryName,
-			"stream_game_id":   event.CategoryID,
-		},
+		bson.M{"$set": bson.M{"stream_title": event.Title, "stream_game_name": event.CategoryName, "stream_game_id": event.CategoryID}},
 	)
 
 	if err != nil {
